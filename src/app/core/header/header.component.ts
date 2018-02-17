489ad47f1401edfbdcc807271329284521ebd54a
Igor Zamozhnii <zamozhnii@gmail.com>
2018-02-17 12:27:10 +0200

    modules

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { Response } from "@angular/http"
-import { DataStorageService } from "../shared/data-storage.service";
-import { AuthService } from "../auth/auth.service";
+import { DataStorageService } from "../../shared/data-storage.service";
+import { AuthService } from "../../auth/auth.service";
 
 @Component({
     selector: 'app-header',
@@ -9,7 +9,7 @@ import { AuthService } from "../auth/auth.service";
 })
 export class HeaderComponent {
     constructor(private dataStorageService: DataStorageService,
-                private authService: AuthService) {}
+                public authService: AuthService) {}
     onSaveData() {
         this.dataStorageService.storeRecipes()
             .subscribe(
---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { AuthGuard } from "../auth/auth-guard.service";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipesComponent } from "./recipes.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+
+const recipesRoutes: Routes = [
+    { path: '', component: RecipesComponent, children: [
+        { path: '', component: RecipeStartComponent},
+        { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
+        { path: ':id', component: RecipeDetailComponent },
+        { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard] }
+    ] },
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(recipesRoutes)
+    ],
+    exports: [
+        RouterModule
+    ],
+    providers: [
+        AuthGuard
+    ]
+}) 
+export class RecipesRoutingModule {
+
+}
